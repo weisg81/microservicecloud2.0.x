c7edbc03d01919897963f6eb9fe4f4885808eca5
weisg81 <weisangeng@126.com>
2018-07-08 21:13:28 +0800

    微服务学习-基于springboot2.0.3版本

---
+++ microservicecloud_config_dept_client_8001/src/main/java/pers/weisg/springcloud/config/HikariDataSourceConfig.java
@@ -0,0 +1,29 @@
+package pers.weisg.springcloud.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/** 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author WEISANGNG   
+ * @date 2018年5月14日   
+ */
+@Configuration
+public class HikariDataSourceConfig {
+
+	@Bean(name = "hikariDataSource")
+	@Qualifier("hikariDataSource")
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public DataSource hikariDataSource() {
+	    System.out.println("Building hikariDataSource......");
+	    DataSourceBuilder<HikariDataSource> type = DataSourceBuilder.create().type(HikariDataSource.class);
+	    //type.password("root");
+	    return type.build();
+	}
+}
---
+++ microservicecloud_provider_dept_hystrix_8001/src/main/java/pers/weisg/springcloud/config/HystrixConfiguration.java
@@ -0,0 +1,24 @@
+package pers.weisg.springcloud.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description 这个类作用是什么
+ * Author WEISANGENG
+ * Date 2018/7/4
+ **/
+@Configuration
+public class HystrixConfiguration {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
