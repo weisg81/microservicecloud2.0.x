1e99fe01d1f5538b0be70191ddca22c9fe0746cc
weisg81 <weisangeng@126.com>
2019-01-07 21:30:37 +0800

    hystrix配置修改

--- microservicecloud_provider_dept_hystrix_8001/src/main/java/pers/weisg/springcloud/controller/DeptController.java
+++ microservicecloud_provider_dept_hystrix_8001/src/main/java/pers/weisg/springcloud/controller/DeptController.java
@@ -1,5 +1,6 @@
 package pers.weisg.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,10 @@ public class DeptController {
 
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
 	// 一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
-	@HystrixCommand(fallbackMethod = "processHystrix_Get")
+//	@HystrixCommand(commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"),
+//            @HystrixProperty(name = "execution.timeout.enabled", value = "true")},fallbackMethod = "processHystrix_Get")
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
 	public Dept get(@PathVariable("id") Long id) {
 
 		Dept dept = this.service.get(id);
